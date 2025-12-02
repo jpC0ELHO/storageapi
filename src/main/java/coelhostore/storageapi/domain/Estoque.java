397cab6d20a804c96ecb4aeda35493956836e330
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-02 19:43:39 -0300

    update Estoque

--- src/main/java/coelhostore/storageapi/domain/Estoque.java
+++ src/main/java/coelhostore/storageapi/domain/Estoque.java
@@ -1,4 +1,36 @@
 package coelhostore.storageapi.domain;
 
-public class Estoque {
+import coelhostore.storageapi.domain.enums.EstoqueTipo;
+import coelhostore.storageapi.domain.enums.TipologiaEstoque;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import tools.jackson.databind.PropertyNamingStrategies;
+import tools.jackson.databind.annotation.JsonNaming;
+
+import java.math.BigDecimal;
+
+@Data
+@Table(name = "tb_estoque")
+@Entity
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Estoque extends Entidade{
+    @ManyToOne
+    @JoinColumn(name = "produto_id",nullable = false)
+    private Produtos produto;
+    @Column(unique = true,length = 10)
+    private String sku;
+    @Column(length = 10, nullable = false, precision = 5, scale = 2)
+    private BigDecimal quantidade;
+    @Column(name = "valor_em_estoque",nullable = false,scale = 100,precision = 5)
+    private BigDecimal valorEmEstoque;
+    //fornecer o tipo de estoque em que esta armazenado
+    @Enumerated(EnumType.STRING)
+    private EstoqueTipo estoqueTipo;
+    //Quantidade definida em unidades de medida
+    @Enumerated(EnumType.STRING)
+    private TipologiaEstoque tipologiaEstoque;
 }
