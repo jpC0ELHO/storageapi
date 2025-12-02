b92780b50eed7ad7089d1602d05d3cc556e0ca45
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-02 19:43:52 -0300

    update Produtos

--- src/main/java/coelhostore/storageapi/domain/Produtos.java
+++ src/main/java/coelhostore/storageapi/domain/Produtos.java
@@ -1,4 +1,28 @@
 package coelhostore.storageapi.domain;
 
-public class Produtos {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import tools.jackson.databind.PropertyNamingStrategies;
+import tools.jackson.databind.annotation.JsonNaming;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_produtos")
+@EntityListeners(AuditingEntityListener.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Produtos extends Entidade{
+    @Column(name = "sku",unique = true,nullable = false)
+    private String sku;
+    @Column(name = "nome_produto", nullable = false)
+    private String nomeProduto;
+    @Column(nullable = false)
+    private String unidadeMedida;
 }
