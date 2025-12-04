50ad7bc1f3835723808072d1da17e99f004ab86c
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-03 22:11:04 -0300

    update Contato

--- src/main/java/coelhostore/storageapi/domain/Contato.java
+++ src/main/java/coelhostore/storageapi/domain/Contato.java
@@ -2,23 +2,14 @@ package coelhostore.storageapi.domain;
 
 import coelhostore.storageapi.domain.enums.TelefoneTipo;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import tools.jackson.databind.PropertyNamingStrategies;
-import tools.jackson.databind.annotation.JsonNaming;
 
 
 @Data
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@Table(name = "tb_contato")
-@AllArgsConstructor
-@Entity
-@EntityListeners(AuditingEntityListener.class)
+@Embeddable
 public class Contato {
     @Column(name = "numero",unique = true,length = 18)
     private String number;
-
     @Enumerated
     @Column(name = "tipo_telefone")
     private TelefoneTipo telefoneTipo;
