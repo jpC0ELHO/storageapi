89877c9e65c6a55e1dfaf1198e79e7e8594eeaed
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-03 22:14:03 -0300

    update Estoque

--- src/main/java/coelhostore/storageapi/domain/Estoque.java
+++ src/main/java/coelhostore/storageapi/domain/Estoque.java
@@ -21,11 +21,9 @@ public class Estoque extends Entidade{
     @ManyToOne
     @JoinColumn(name = "produto_id",nullable = false)
     private Produtos produto;
-    @Column(unique = true,length = 10)
-    private String sku;
     @Column(length = 10, nullable = false, precision = 5, scale = 2)
     private BigDecimal quantidade;
-    @Column(name = "valor_em_estoque",nullable = false,scale = 100,precision = 5)
+    @Column(name = "valor_em_estoque",nullable = false,scale = 2,precision = 12)
     private BigDecimal valorEmEstoque;
     //fornecer o tipo de estoque em que esta armazenado
     @Enumerated(EnumType.STRING)
@@ -33,4 +31,6 @@ public class Estoque extends Entidade{
     //Quantidade definida em unidades de medida
     @Enumerated(EnumType.STRING)
     private TipologiaEstoque tipologiaEstoque;
+    @Column(nullable = false)
+    private BigDecimal quantidadeReservada;
 }
