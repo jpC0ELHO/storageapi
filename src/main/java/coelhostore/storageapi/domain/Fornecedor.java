60c74f5bbed665f792fd7daa05ab278782120bc8
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-03 22:14:44 -0300

    update Fornecedor

--- src/main/java/coelhostore/storageapi/domain/Fornecedor.java
+++ src/main/java/coelhostore/storageapi/domain/Fornecedor.java
@@ -1,4 +1,19 @@
 package coelhostore.storageapi.domain;
 
+import coelhostore.storageapi.domain.enums.DocumentoTipo;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Embeddable
 public class Fornecedor {
+    @Column(nullable = false)
+    private String nome;
+    @Enumerated(EnumType.STRING)
+    private DocumentoTipo documentoTipo;
+    @Embedded
+    private Contato contato;
+    @ManyToOne
+    @JoinColumn(name = "endereco_id", nullable = false)
+    private Endereco endereco;
 }
