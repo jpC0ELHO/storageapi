4058e1760d9bb553baee313087250dd6e3bd7478
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-10 20:01:38 -0300

    update EstoqueSaida

--- src/main/java/coelhostore/storageapi/domain/EstoqueSaida.java
+++ src/main/java/coelhostore/storageapi/domain/EstoqueSaida.java
@@ -1,11 +1,19 @@
 package coelhostore.storageapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 import tools.jackson.databind.PropertyNamingStrategies;
+import tools.jackson.databind.annotation.JsonDeserialize;
 import tools.jackson.databind.annotation.JsonNaming;
+import tools.jackson.databind.annotation.JsonSerialize;
+import tools.jackson.databind.ext.javatime.deser.LocalDateDeserializer;
+import tools.jackson.databind.ext.javatime.deser.LocalDateTimeDeserializer;
+import tools.jackson.databind.ext.javatime.ser.LocalDateSerializer;
+import tools.jackson.databind.ext.javatime.ser.LocalDateTimeSerializer;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -28,7 +36,10 @@ public class EstoqueSaida extends Entidade{
     private BigDecimal transporteValor;
     @Column(precision = 10, scale = 2)
     private BigDecimal imposto;
-    @Column(nullable = false)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(pattern = "dd/MM/yyyy", shape = JsonFormat.Shape.STRING)
     private LocalDate dataSaida;
     @Column(nullable = false)
     private String motivoSaida; // VENDA, CONSUMO, PERDA, AJUSTE, RESERVA
