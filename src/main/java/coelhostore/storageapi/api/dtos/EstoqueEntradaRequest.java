0474406284b254ae430aea6b1b42ad2fe32fc440
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-12 18:20:15 -0300

    update EstoqueEntradaRequest

--- src/main/java/coelhostore/storageapi/api/dtos/EstoqueEntradaRequest.java
+++ src/main/java/coelhostore/storageapi/api/dtos/EstoqueEntradaRequest.java
@@ -1,4 +1,35 @@
 package coelhostore.storageapi.api.dtos;
 
-public record EstoqueEntradaRequest() {
+import coelhostore.storageapi.domain.EstoqueEntrada;
+import coelhostore.storageapi.domain.Fornecedor;
+import coelhostore.storageapi.domain.Produtos;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record EstoqueEntradaRequest(
+        Produtos produtos,
+        Fornecedor fornecedor,
+        BigDecimal quantidade,
+        BigDecimal valorUnidade,
+        BigDecimal impostos,
+        LocalDate  dataEntrada,
+        BigDecimal valorTotalBruto,
+        BigDecimal valorTotalLiquido
+) {
+    public static EstoqueEntrada toEntidade(EstoqueEntradaRequest estoqueEntradaRequest){
+        if (estoqueEntradaRequest==null){
+            return null;
+        }
+        return new EstoqueEntrada(
+                estoqueEntradaRequest.produtos,
+                estoqueEntradaRequest.fornecedor,
+                estoqueEntradaRequest.quantidade,
+                estoqueEntradaRequest.valorUnidade,
+                estoqueEntradaRequest.impostos,
+                estoqueEntradaRequest.dataEntrada,
+                estoqueEntradaRequest.valorTotalBruto,
+                estoqueEntradaRequest.valorTotalLiquido
+        );
+    }
 }
