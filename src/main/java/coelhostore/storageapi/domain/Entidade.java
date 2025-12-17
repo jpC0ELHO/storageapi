1f6464c17f3ca00d9d46f4b73281f8033a35b786
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-17 20:19:23 -0300

    updated Entidade

--- src/main/java/coelhostore/storageapi/domain/Entidade.java
+++ src/main/java/coelhostore/storageapi/domain/Entidade.java
@@ -2,12 +2,9 @@ package coelhostore.storageapi.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.*;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 import tools.jackson.databind.PropertyNamingStrategies;
@@ -22,31 +19,33 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity
-@EntityListeners(AuditingEntityListener.class)
-@NoArgsConstructor
+
+
+
 @Data
 @MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class )
 public abstract class Entidade implements Serializable {
     @Serial
     private static final long SerialVersionUID=1L;
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
     private UUID uuid;
     @LastModifiedBy
     private String lastModifiedBy;
     @CreatedBy
     private String createdBy;
     @CreatedDate
-    @Temporal(TemporalType.DATE)
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd/MM/yyyy HH:mm:ss")
     @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss" )
     private LocalDateTime createdAt;
     @LastModifiedDate
-    @Temporal(TemporalType.DATE)
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd/MM/yyyy HH:mm:ss")
