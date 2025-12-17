26214e7d18cc65b6429dfa569110e49155197067
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-12-17 20:19:10 -0300

    updated Endereco

--- src/main/java/coelhostore/storageapi/domain/Endereco.java
+++ src/main/java/coelhostore/storageapi/domain/Endereco.java
@@ -3,10 +3,10 @@ package coelhostore.storageapi.domain;
 import coelhostore.storageapi.domain.enums.EstadoBr;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import tools.jackson.databind.PropertyNamingStrategies;
 import tools.jackson.databind.annotation.JsonNaming;
@@ -15,24 +15,24 @@ import tools.jackson.databind.annotation.JsonNaming;
 @EntityListeners(AuditingEntityListener.class)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class Endereco {
+public class Endereco extends Entidade{
     @Column(nullable = false, unique = false, length = 100)
     private String logradouro;
     @Column(nullable = false, unique = false, length = 10)
     private String numero;
-    @Column(nullable = false, unique = false, length = 100)
+    @Column(unique = false, length = 100)
     private String complemento;
     @Column(nullable = false, unique = false, length = 100)
     private String bairro;
     @Column(nullable = false, unique = false, length = 100)
     private String cidade;
-    @Column(nullable = false, unique = false, length = 20)
+    @Enumerated
     private EstadoBr estado;
-    @NotNull
     @NotBlank
-    @Column(nullable = false, unique = false, length = 10)
+    @Column(nullable = false, unique = false, length = 9)
     @Pattern(regexp = "\\d{5}-\\d{3}", message = "O campo 'cep' deve ter o formato '12345-678'")
     private String cep;
 }
